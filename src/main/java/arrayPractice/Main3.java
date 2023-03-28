9188c48d6ebe3c01fd971d0bcf843416ab296a17
kCoder <114634268+mKeertika@users.noreply.github.com>
2023-03-27 19:09:53 -0500

    Complete 303.3.2 Practice Assignment -Array

--- src/main/java/arrayPractice/Main3.java
+++ src/main/java/arrayPractice/Main3.java
@@ -1,2 +1,21 @@
-package arrayPractice;public class Main3 {
+package arrayPractice;
+/*Write a program that creates an array of String type and initializes it with the
+ strings “red”, “green”, “blue” and “yellow”. Print out the array length. Make a
+ copy using the clone( ) method. Use the Arrays.toString( ) method on the new
+ array to verify that the original array was copied.*/
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Main3 {
+    public static void main(String[] args) {
+
+        //String type and initializes it with the strings
+        String[] colorArr ={"red", "green","blue", "yellow"};
+        System.out.println("length of the array is: "+ colorArr.length);
+        //clone the array
+        String [] cloneColorArr= colorArr.clone();
+        System.out.println("New CLONED array is: "+ Arrays.toString(cloneColorArr));
+
+    }
 }
