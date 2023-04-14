78cbebc9237193797e8da416b11d4cb74f7660eb
kCoder <114634268+mKeertika@users.noreply.github.com>
2023-04-13 22:43:40 -0500

    Done with Arraylist Lab except Iteration

--- src/main/java/collection/RemoveElementsFromArrayList.java
+++ src/main/java/collection/RemoveElementsFromArrayList.java
@@ -1,7 +1,43 @@
 package collection;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RemoveElementsFromArrayList {
     public static void main(String[] args) {
+
+        List<String> programmingLanguage = new ArrayList<>();
+        programmingLanguage.add("C");
+        programmingLanguage.add("C++");
+        programmingLanguage.add("Java");
+        programmingLanguage.add("Kotlin");
+        programmingLanguage.add("Python");
+        programmingLanguage.add("Pearl");
+        programmingLanguage.add("Ruby");
+
+        System.out.println("Initial List: " + programmingLanguage);
+
+//        remove index at index 5
+        programmingLanguage.remove(5);
+        System.out.println("List after edit index at 5: "+programmingLanguage);
+
+//        remove object
+        boolean isRemove= programmingLanguage.remove("Kotlin");
+        System.out.println("whether Kotlin exists in List: "+ isRemove);
+        System.out.println("After removing Kotlin in list" + programmingLanguage);
+
+//        Remove all
+        List<String> scriptingLanguage= new ArrayList<>();
+        scriptingLanguage.add("Python");
+        scriptingLanguage.add("Ruby");
+        scriptingLanguage.add("Pearl");
+
+        programmingLanguage.removeAll(scriptingLanguage);
+        System.out.println("List after remove all scripting language: "+ programmingLanguage);
+
+//        remove all elements from list
+        programmingLanguage.clear();
+        System.out.println("List after using clear() method: "+ programmingLanguage);
+
 
     }
 }
---
+++ src/main/java/collection/SearchElementsInArrayListExample.java
@@ -0,0 +1,30 @@
+package collection;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchElementsInArrayListExample {
+    public static void main(String[] args) {
+
+        List<String> names = new ArrayList<>();
+        names.add("John");
+        names.add("Alice");
+        names.add("Bob");
+        names.add("Steve");
+        names.add("John");
+        names.add("Steve");
+        names.add("Maria");
+        names.add("Maria");
+
+        // Check if an ArrayList contains a given element
+        System.out.println("Does names array contain \"Bob\"? : "+ names.contains("Bob"));
+
+        // Find the index of the first occurrence of an element in an ArrayList
+        System.out.println("indexOf \"Steve\": " + names.indexOf("Steve") );
+        System.out.println("indexOf \"Maria\": " + names.indexOf("Maria") );
+        System.out.println("indexOf \"Keertika\": " + names.indexOf("Keertika") );
+
+        // Find the index of the last occurrence of an element in an ArrayList
+        System.out.println("lastIndexOf \"Maria\" : " + names.lastIndexOf("Maria"));
+        System.out.println("lastIndexOf \"Keertika\" : " + names.lastIndexOf("Keertika"));
+    }
+}
