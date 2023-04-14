fcaadd43207aa1ceb3883a7f053d5563132c6953
kCoder <114634268+mKeertika@users.noreply.github.com>
2023-04-13 23:26:15 -0500

    linked list Lab Done

--- src/main/java/collection/LinkedListExample.java
+++ src/main/java/collection/LinkedListExample.java
@@ -19,5 +19,31 @@ public class LinkedListExample {
         linkedlist.addFirst("Random Item");
         System.out.println("now linked list after adding random item:" + linkedlist);
 
+        /*Display Linked List Content*/
+        System.out.println("Linked List Content: " +linkedlist);
+
+        /*Add First and Last Element*/
+        linkedlist.addFirst("First Item");
+        linkedlist.addLast("Last Item");
+        System.out.println("LinkedList Content after addition: " +linkedlist);
+
+        /*This is how to get and set Values*/
+        Object firstVar = linkedlist.get(0);
+        System.out.println("First element: " +firstVar);
+        linkedlist.set(0, "Changed first item");
+        Object firstVar2 = linkedlist.get(0);
+        System.out.println("First element after update by set method: " +firstVar2);
+
+
+        /*Remove first and last element*/
+        linkedlist.removeFirst();
+        linkedlist.removeLast();
+        System.out.println("LinkedList after deletion of first and last element: " +linkedlist);
+
+        /* Add to a Position and remove from a position*/
+        linkedlist.add(0, "Newly added item");
+        linkedlist.remove(2);
+        System.out.println("Final Content: " +linkedlist);
+
     }
 }
